38af6445dfcd8c8cde818b64ed41647693e6410d
Joaquin Failo <73504599+Joafai@users.noreply.github.com>
2025-06-17 15:07:30 -0300

    First commit
    set up project

--- contracts.ts
+++ contracts.ts
@@ -1,10 +1,20 @@
 import type { Contracts } from "./.lib/types";
 
 export default {
+  //poolExposureSteward
   mainnet: {
-    weth: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-    uniswap: {
-      positions_nft: "0xC36442b4a4522E871399CD717aBDD847Ab11FE88",
-    },
+    pes: "0x22aC12a6937BBBC0a301AF9154d08EaD95673122" as `0x${string}`,
+  },
+  arbitrumOne: {
+    pes: "0xfB1D12D7C9c3Eb6b40fe0502801CdFCE816a0d18" as `0x${string}`,
+  },
+  polygon: {
+    pes: "0xE5b11ab4D36E58C9171e3DB98Ba17336606Cd6ef" as `0x${string}`,
+  },
+  optimism: {
+    pes: "0x4684b645386525057526EB85854045914Fe389d6" as `0x${string}`,
+  },
+  base: {
+    pes: "0x4f1F3E32f20847a4c9F002882eB607aD750b6115" as `0x${string}`,
   },
 } satisfies Contracts;
---
+++ roles/aave_depositor/members.ts
@@ -0,0 +1,3 @@
+export default [
+  "0x0Fa7b2b8D25D27FC4c5C2B5802246D8900e28E0C"
+] satisfies `0x${string}`[];
